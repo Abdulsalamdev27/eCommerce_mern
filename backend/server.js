ad547202db3f6e792c97579bd1cb2ae5be66e37e
abubakadev27 <abubakaabdulsalam691@gmail.com>
2026-04-06 13:53:05 -0700

    adding coupon for discount

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponsRoutes from "./routes/coupon.route.js";
 import { connectDB } from "./lib/db.js";
 
 
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 8080;
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
+app.use("/api/coupons", couponsRoutes)
 
 
 app.listen(5000, ()=>{
